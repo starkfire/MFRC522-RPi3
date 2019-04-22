5ad5002ba3de247c5eb061be8eca1c8e2a480eb5
Pocholo Pantoja <kingbeyondthenorth@gmail.com>
2019-04-22 15:51:18 +0800

    Fetching UID

--- tango.py
+++ tango.py
@@ -1,12 +1,14 @@
 # Tap-and-Go (Tango) Script
 # automates the fare payment procedure
 
-from cards import CardBalanceReducer
-from module.base import BaseCard, CardWriter
+from cards import CardBalanceReducer, CardBalanceReader
+from module.base import BaseCard
 import base
 
-import numpy as np
+import time
+import csv
 import pandas as pd
+import numpy as np
 import math
 
 def check_card_present(obj):
@@ -27,9 +29,18 @@ def haversine(pair1, pair2):
 	haversine_distance = 2*R*math.atan2(math.sqrt(a), math.sqrt(1-a))
 	return haversine_distance
 
+# needs further evaluation and a lot of refactoring
+# i don't have device access now, so i might not be able to maintain this
+# btw, had lots of issues
 def main():
+	# Log file references
+	gpsl_log = 'logs/GPSLogger/gpslogger.csv'
+	location_log = 'logs/location.csv'
+	inbound_log = 'logs/inbound.csv'
 	while True:
-		card = CardBalanceReducer()
+		outbound = None
+		# snippet
+		card = CardBalanceReader()
 		if not check_card_present(card):
 			print("Place card to reader")
 		while not check_card_present(card):
@@ -37,8 +48,7 @@ def main():
 		# get UID
 		UID = card.get_uid().get('uid')
 		tail_uid = str(UID[0]) + str(UID[1]) + str(UID[2]) + str(UID[3]) + str(UID[4])
-		# still have to add the other functions and stuff, I have to test them atm
-
+			
 if __name__ == '__main__':
 	try:
 		main()
