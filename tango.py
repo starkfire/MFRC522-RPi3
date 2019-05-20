fa078bbe705d7c0ecce6b51a901e9624ec210e71
Pocholo Pantoja <kingbeyondthenorth@gmail.com>
2019-05-20 16:17:34 +0800

    magbayad kayo

--- tango.py
+++ tango.py
@@ -23,7 +23,7 @@ def haversine(pair1, pair2):
 	lat1, lon1 = pair1
 	lat2, lon2 = pair2
 	phi1, phi2 = math.radians(lat1), math.radians(lat2)
-	dphi = math.radians(lat2 - lat)
+	dphi = math.radians(lat2 - lat1)
 	dlambda = math.radians(lon2 - lon1)
 	a = math.sin(dphi/2)**2 + math.cos(phi1)*math.cos(phi2)*math.sin(dlambda/2)**2
 	haversine_distance = 2*R*math.atan2(math.sqrt(a), math.sqrt(1-a))
